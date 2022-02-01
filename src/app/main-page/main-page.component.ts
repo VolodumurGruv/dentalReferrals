56043c52a652a0759510710830e1198c280a3203
VolodumurGruv <gru.v@i.ua>
2022-02-01 16:39:34 +0200

    last commit

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { mockData } from '../mockData/mockData';
 import { MockData } from '../mockData/mockData.interface';
@@ -7,6 +7,7 @@ import { MockData } from '../mockData/mockData.interface';
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MainPageComponent implements OnInit {
   public userInfo: MockData = mockData;
